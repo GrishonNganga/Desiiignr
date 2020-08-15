c3b7c5db6583fb9c655e493809ef380069c35ff7
Grishon Nganga <52824774+GrishonNganga@users.noreply.github.com>
2020-08-15 19:14:13 +0300

    Add template tags to show people who have liked.

--- insta/templatetags/like.py
+++ insta/templatetags/like.py
@@ -1,5 +1,6 @@
 from django import template
 from django.contrib.auth import get_user_model
+import random
 
 User = get_user_model()
 
@@ -16,3 +17,21 @@ def check_like(post_id, user_id):
         return False
     else:
         return True
+
+@register.simple_tag
+def liked_by(post_id):
+    likes = Like.objects.filter(post = post_id)
+    number_guess_range = len(likes)
+    if number_guess_range == 0:
+        return None
+    elif number_guess_range == 1:
+        return likes[0].username
+    else:
+        selected_no = random.randint(0, number_guess_range -1)
+        print(selected_no)
+        return likes[selected_no].username
+        
+
+@register.simple_tag
+def others(post_id):
+    return len(Like.objects.filter(post = post_id)) -1
